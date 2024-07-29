12b529e9973387743ebec406ce8a7f0055087121
Daniil Savko <135634324+zzoddiaak@users.noreply.github.com>
2024-07-29 14:17:12 +0300

    Update exercise8.sql

--- Joins/exercise8.sql
+++ Joins/exercise8.sql
@@ -24,15 +24,15 @@ CREATE TABLE bookings(
     slots integer
 );
 
-insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(0, 'Tennis Court 1', 5, 25, 10000, 200);
-insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(1, 'Tennis Court 2', 5, 25, 8000, 200);
-insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(2, 'Badminton Court', 0, 15.5, 4000, 50);
-insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(3, 'Table Tennis', 0, 5, 320, 10);
-insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(4, 'Massage Room 1', 35, 80, 4000, 3000);
-insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(5, 'Massage Room 2', 35, 80, 4000, 3000);
-insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(6, 'Squash Court', 3.5, 17.5, 5000, 80);
-insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(7, 'Snooker Table', 0, 5, 450, 15);
-insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(8, 'Pool Table', 0, 5, 400, 15);
+insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(0,'Tennis Court 1', 5, 25, 10000, 200);
+insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(1,'Tennis Court 2', 5, 25, 8000, 200);
+insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(2,'Badminton Court', 0, 15.5, 4000, 50);
+insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(3,'Table Tennis', 0, 5, 320, 10);
+insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(4,'Massage Room 1', 35, 80, 4000, 3000);
+insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(5,'Massage Room 2', 35, 80, 4000, 3000);
+insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(6,'Squash Court', 3.5, 17.5, 5000, 80);
+insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(7,'Snooker Table', 0, 5, 450, 15);
+insert into faсilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values(8,'Pool Table', 0, 5, 400, 15);
 insert into bookings(bookid, facid, memid, starttime, slots) values(2936,4,14,'2012-09-14 08:00:00',2);
 insert into bookings(bookid, facid, memid, starttime, slots) values(2937,4,0,'2012-09-14 09:00:00',2);
 insert into bookings(bookid, facid, memid, starttime, slots) values(2945,5,15,'2012-09-14 09:30:00',2);
